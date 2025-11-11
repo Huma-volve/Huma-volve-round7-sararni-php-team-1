6ed58ae4dd9421f56b78696c5bed27d2b946a530
israa <israairfan99@gmail.com>
2025-11-11 23:05:25 +0200

    Add models

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
+use App\Models\Car;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class Location extends Model
 {
     //
+    use SoftDeletes;
+
+    protected $fillable = ['city','country','latitude','longitude'];
+
+    public function pickupCars()
+    {
+        return $this->hasMany(Car::class, 'pickup_location_id');
+    }
+
+    public function dropoffCars()
+    {
+        return $this->hasMany(Car::class, 'dropoff_location_id');
+    }
+
 }
