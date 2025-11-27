bd39cd299a0f3e1c9c9c50f11b02483a2c9a2c8b
Eslam Mohamed <eslamemam39@gmail.com>
2025-11-27 21:26:16 +0200

    Add payment integration improvements

--- routes/Api/v1/questions.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Http\Controllers\Api\V1\QuestionController;
-use Illuminate\Support\Facades\Route;
-
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/', [QuestionController::class, 'store']);
-    Route::post('/{id}/answer', [QuestionController::class, 'answer']);
-});
